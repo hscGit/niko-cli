5a6ef53ad576ac0f5725d7837004dddac544d9e8
hscer <2862664216@qq.com>
2023-04-03 10:08:29 +0800

    refactor:修改项目模版json

--- commands/init/lib/getProjectTemplate.js
+++ commands/init/lib/getProjectTemplate.js
@@ -1,9 +1,19 @@
 'use strict';
 
-const request = require('@niko-cli/request');
-
 module.exports = function () {
-    return request({
-        url: '/project/template'
-    })
+    return [
+        {
+            _id: {
+                $oid: "642695c0f6a1c59ef90d7995"
+            },
+            npmName: "niko-cli-template-react18",
+            tag: "['project']",
+            version: "0.1.0",
+            name: "react18标准模版",
+            type: "normal",
+            installCommand: "npm install",
+            ignore: "['**/public/**']",
+            startCommand: "npm run dev"
+        }
+    ]
 }
